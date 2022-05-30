54a93878f320c8c9760eec785223d4384d4719b3
rodrigo araujo santos <araujo.1983@hotmail.com>
2022-05-30 17:53:48 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'order', to: 'pages#order'
+  get 'sneaker', to: 'pages#sneaker'
+  get 'user', to: 'pages#user'
+  
 end
