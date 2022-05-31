3905ea680b7a9ff118a3b6b2cae4838bd11d20de
rodrigo araujo santos <araujo.1983@hotmail.com>
2022-05-31 18:35:41 -0300

    toBuy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+  def create
+    @order = Order.new
+    @order.user = current_user
+    @order.sneaker = Sneaker.find(params[:sneaker_id])
+    if @order.save
+      redirect_to sneakers_path, alert: "Congratulation!!!"
+    else
+      render Sneaker.find(params[:sneaker_id]), alert: "We couldn't complete your order"
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :sneakers do
-    resources :orders, only: %i[new create edit]
+    resources :orders, only: %i[create]
   end
 
 end
