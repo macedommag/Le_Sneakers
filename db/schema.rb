46543556db65b5a25ca7196be388f9965db69594
Gabriel Magalhaes <macedommag@gmail.com>
2022-06-01 12:19:49 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-  # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2022_05_31_161858) do
     t.string "brand"
     t.string "condition"
     t.integer "price"
-    t.integer "amount"
+    t.integer "inventory"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
