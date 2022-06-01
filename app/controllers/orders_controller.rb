fa726c4adc6b9674e84472a8ea2db4469a9c166b
Renan Dantas <rsdantas7@gmail.com>
2022-06-01 11:46:52 -0300

    alteracao_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @sneaker = Sneaker.find(params[:sneaker_id])
+    @order = Order.new(sneaker: @sneaker, user: current_user)
+    @order.save
+    redirect_to order_path(@order)
+  end
+
+  def destroy
+  end
 end
